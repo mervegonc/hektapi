3eea44f939a8b7e72be63a2e055de69970f2178f
merve-goncu <merve.gnc.946@gmail.com>
2026-07-16 13:33:37 +0300

    fix:config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   images: {
@@ -13,15 +12,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  org: "hektapi",           // sentry.io'daki organization slug'ın
-  project: "hektapi-nextjs", // sentry.io'daki proje adın
-
-  // Source map'leri Sentry'e yükle (hata satırlarını görmek için)
-  silent: true,
-
-  // Vercel üzerinde otomatik instrumentation
-  widenClientFileUpload: true,
-  disableLogger: true,
-  automaticVercelMonitors: false,
-});
+export default nextConfig;
